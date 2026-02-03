74039ded512858a49c811a5cc1cb2cd8aaffff9f
vizssouza <vini.nelso@gmail.com>
2026-02-03 15:13:48 -0300

    Adicionando arquivos iniciais

---
+++ JS/contador.js
@@ -0,0 +1,11 @@
+let numero = 0;
+const max = 30;
+
+const intervalo = setInterval(() => {
+    document.getElementById("contador").innerText = numero;
+    numero++;
+
+    if (numero > max) {
+        clearInterval(intervalo);
+    }
+}, 30);
